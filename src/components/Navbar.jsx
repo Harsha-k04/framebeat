0c486792aca05688750cff26950cda0e3055e9b6
Harsha <harsha.nair04@gmail.com>
2026-01-09 13:54:18 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,6 +26,10 @@ export default function Navbar() {
           <Link to="/case-studies" className="hover:text-white">
             Case Studies
           </Link>
+          <a href="/careers" className="hover:opacity-70 transition">
+            Careers
+          </a>
+
           <Link
             to="/contact"
             className="rounded-lg bg-white px-4 py-2 text-black hover:bg-neutral-200"
